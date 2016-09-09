LineageOS/android_device_huawei_angler

Fixed thermal HAL, added skin temperature, thresholds.
Bug: 27424857
Change-Id: Ia28f9de4305523bc6149f3f1d12fad400df15370

thermal/Android.mk
@@ -19,8 +19,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -Wno-unused-parameter
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := hardware_properties.c
+LOCAL_SRC_FILES := thermal.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := hardware_properties.angler
+LOCAL_MODULE := thermal.angler
 include $(BUILD_SHARED_LIBRARY)

PMS22/common

thermal/Android.mk
@@ -19,8 +19,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_CFLAGS := -Wno-unused-parameter
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SRC_FILES := hardware_properties.c
+LOCAL_SRC_FILES := thermal.c
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := hardware_properties.msm8916
+LOCAL_MODULE := thermal.msm8916
 include $(BUILD_SHARED_LIBRARY)