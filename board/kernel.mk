d24a1c9efb910c6134abb4c78bd79db653ff713a
PMS22 <prathams99@rediff.com>
2016-09-16 10:45:00 +0000

    Fixed TC

--- board/kernel.mk
+++ board/kernel.mk
@@ -11,7 +11,7 @@ ENABLE_CPUSETS := true
 
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
 ifneq ($(FORCE_32_BIT),true)
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9-kernel/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_HEADER_ARCH := arm64
